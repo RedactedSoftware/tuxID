17e7e015c61998ddd70d29917a1082e81746bd8f
scientiist <j0sh.oleary11@gmail.com>
2023-03-23 09:56:53 -0500

    Allow more robust disk poking.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 int main()
 {
     std::cout << "HardwareID Demo" << std::endl;
-    std::cout << "Disk Serial Code: " << Stepbro::HardwareID::getDiskSerialCode() << std::endl;
+    std::cout << "Disk Serial Code (SATA): " << Stepbro::HardwareID::getDiskSerialCode("/dev/sda")    << std::endl;
+    std::cout << "Disk Serial Code (IDE): "  << Stepbro::HardwareID::getDiskSerialCode("/dev/hda")    << std::endl;
+    std::cout << "Disk Serial Code (NVMe): " << Stepbro::HardwareID::getDiskSerialCode("/dev/nvme")   << std::endl;
+    std::cout << "Disk Serial Code (eMMC): " << Stepbro::HardwareID::getDiskSerialCode("/dev/mmcblk") << std::endl;
     std::cout << "VM Detected: " << Stepbro::HardwareID::isVirtualMachine() << std::endl;
    return 0;
 }
