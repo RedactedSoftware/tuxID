9e264905d7f2fb502a5f3b0bded479ace241ffca
scientiist <j0sh.oleary11@gmail.com>
2023-03-24 18:50:45 -0500

    A little more documentation :)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "hardwareID.hpp"
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
 
 int main()
 {
     std::cout << OBFUSCATE("HardwareID Demo") << std::endl;
-    std::cout << OBFUSCATE("BIOS Vendor: ") << tuxID::readDMIData("Vendor:") << std::endl;
+    // TODO: Implement string::trim
+    std::cout << tuxID::readDMIData("Currently Installed Language:");
+    std::cout << tuxID::readDMIData("Range Size:");
+    std::cout << tuxID::readDMIData("Family");
+    std::cout << tuxID::readDMIData("Socket");
+
+    std::cout << OBFUSCATE("BIOS: ") << tuxID::readDMIData("Vendor:") << ".";
     std::cout << OBFUSCATE("Disk Serial Code: ") << tuxID::getDiskSerialCode()  << std::endl;
     std::cout << OBFUSCATE("Is SuperUser: ") << tuxID::isSuperUser() << std::endl;
     std::cout << OBFUSCATE("VM Detected: ") << tuxID::isVirtualMachine() << std::endl;
