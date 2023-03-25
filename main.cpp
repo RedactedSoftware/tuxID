0582f77923d27aee67e62efe62449da21a269fee
NotStepbro <notstepbro@crazilyfinger.me>
2023-03-25 01:42:55 -0400

    Update main.cpp
    Organization.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,11 @@ int main()
     std::cout << OBFUSCATE("Motherboard Vendor: ") << tuxID::getFileContents(std::string(OBFUSCATE("/sys/devices/virtual/dmi/id/sys_vendor"))) << std::endl;
     std::cout << OBFUSCATE("Is SuperUser: ") << tuxID::isSuperUser() << std::endl;
     std::cout << OBFUSCATE("VM Detected: ") << tuxID::isVirtualMachine() << std::endl;
+    if (tuxID::getFileContents(std::string(OBFUSCATE("/sys/devices/virtual/dmi/id/product_name"))) == std::string(OBFUSCATE("KVM")))
+        std::cout << std::string (OBFUSCATE("Virtual Machine Type: KVM")) << std::endl;
+    if (tuxID::getFileContents(std::string(OBFUSCATE("/sys/devices/virtual/dmi/id/product_name"))) == std::string(OBFUSCATE("VirtualBox")))
+        std::cout << std::string (OBFUSCATE("Virtual Machine Type: VirtualBox")) << std::endl;
     std::cout << OBFUSCATE("Debugger Attached: ") << tuxID::isDebuggerAttached() << std::endl;
     std::cout << OBFUSCATE("LD_PRELOAD: ") << tuxID::isLDPreload() << std::endl;
-    if (tuxID::getFileContents(std::string(OBFUSCATE("/sys/devices/virtual/dmi/id/product_name"))) == std::string(OBFUSCATE("KVM"))) {
-        std::cout << std::string (OBFUSCATE("Virtual Machine Type: KVM")) << std::endl;
-    }
-    if (tuxID::getFileContents(std::string(OBFUSCATE("/sys/devices/virtual/dmi/id/product_name"))) == std::string(OBFUSCATE("VirtualBox"))) {
-        std::cout << std::string (OBFUSCATE("Virtual Machine Type: VirtualBox")) << std::endl;
-    }
    return 0;
 }
